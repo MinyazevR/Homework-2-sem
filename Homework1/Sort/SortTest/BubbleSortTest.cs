3e677e7ce3bede7b4cbbb8f851ef1a0fe3bee50f
MinyazevR <eeuriset8@gmail.com>
2022-02-21 17:22:43 +0300

    add Tests

--- Homework1/Sort/SortTest/BubbleSortTest.cs
+++ Homework1/Sort/SortTest/BubbleSortTest.cs
@@ -8,15 +8,57 @@ namespace SortTest
     public class BubbleSortTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckSortArrayofEqualNumbers()
         {
             int[] arrayOfNumbers = new int[100];
+            for (int i = 1; i < arrayOfNumbers.Length; i++)
+            {
+                arrayOfNumbers[i] = 12;
+            }
             BubbleSort array = new BubbleSort();
             array.BubbleSortArray(arrayOfNumbers);
             int flag = 1;
             for (int i = 1; i < arrayOfNumbers.Length; i++)
             {
                 if(arrayOfNumbers[i] < arrayOfNumbers[i - 1])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            Assert.AreEqual(flag, 1);
+        }
+        [TestMethod]
+        public void CheckSortOfSortedArray()
+        {
+            int[] arrayOfNumbers = new int[100];
+            for (int i = 1; i < arrayOfNumbers.Length; i++)
+            {
+                arrayOfNumbers[i] = i;
+            }
+            BubbleSort array = new BubbleSort();
+            array.BubbleSortArray(arrayOfNumbers);
+            int flag = 1;
+            for (int i = 1; i < arrayOfNumbers.Length; i++)
+            {
+                if (arrayOfNumbers[i] < arrayOfNumbers[i - 1])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            Assert.AreEqual(flag, 1);
+        }
+        [TestMethod]
+        public void CheckSortOfEmptyArray()
+        {
+            int[] arrayOfNumbers = Array.Empty<int>();
+            BubbleSort array = new BubbleSort();
+            array.BubbleSortArray(arrayOfNumbers);
+            int flag = 1;
+            for (int i = 1; i < arrayOfNumbers.Length; i++)
+            {
+                if (arrayOfNumbers[i] < arrayOfNumbers[i - 1])
                 {
                     flag = 0;
                     break;

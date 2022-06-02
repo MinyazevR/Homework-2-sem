fe398718b49ddb6ee1e00cfd5e4c95eb5f94ded0
Roman <eeuriset8@gmail.com>
2022-06-02 03:03:49 +0300

    formatted

--- List/List/List/List/RemoveNonExistingElementException.cs
+++ List/List/List/List/RemoveNonExistingElementException.cs
@@ -3,4 +3,4 @@
 /// <summary>
 /// Exception for the case of deleting a non-existent element
 /// </summary>
-public class RemoveNonExistingElementException : InvalidOperationException{ }
+public class RemoveNonExistingElementException : InvalidOperationException { }
--- List/List/List/List/RepeatValueException.cs
+++ List/List/List/List/RepeatValueException.cs
@@ -3,4 +3,4 @@
 /// <summary>
 /// Exception for the case of adding an existing element
 /// </summary>
-public class RepeatValueException : InvalidOperationException{ }
+public class RepeatValueException : InvalidOperationException { }
